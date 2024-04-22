963de9db02b94fd60315bf25df36dc0f0a34a440
stefandobrosavljevic <stefipanter999@gmail.com>
2024-04-22 18:32:23 +0200

    Dopunjen zadatak sa komentarima i optimizacija koda

--- Zadatak/zadatak.py
+++ Zadatak/zadatak.py
@@ -1,16 +1,31 @@
-import math
 import numpy as np
 
 
-def loadData(data_file):
-    with open(data_file, 'r') as file:
-        points = [tuple(map(float, line.strip().split(","))) for line in file]
+def load_data(data_file):
+    try:
+        with open(data_file, 'r') as file:
+            points = [tuple(map(float, line.strip().split(",")))
+                      for line in file]
+        if len(points) != 4:
+            raise ValueError("Fajl mora da ima tacno 4 tačke")
         return points
+    except Exception as e:
+        print(f"Greška prilikom učitavanja podataka {e}")
+        return None
 
 
-def checkRectagleFromPoints(A, B, C):
+def is_right_angle(p1, p2, p3):
     """
-    # 
+    Odredjuje da li je ugao izmedju A-B-C prav ugao (90 stepeni u tacki B)
+    """
+    v1 = np.array(p2) - np.array(p1)
+    v2 = np.array(p2) - np.array(p3)
+    return np.isclose(np.dot(v1, v2), 0)
+
+
+def check_rectagle_from_points(A, B, C):
+    """
+    Kod pomocu pitagorine teoreme
     if A == B or A == C or B == C:
         return False
     d1 = (A[0] - B[0])**2 + (A[1] - B[1])**2
@@ -22,63 +37,54 @@ def checkRectagleFromPoints(A, B, C):
 
     """
 
-    return (isRightAngle(A, B, C) or isRightAngle(B, A, C) or isRightAngle(A, C, B))
+    return (is_right_angle(A, B, C) or is_right_angle(B, A, C) or is_right_angle(A, C, B))
 
 
-def isRightAngle(A, B, C):
-    v1 = np.array(B) - np.array(A)
-    v2 = np.array(B) - np.array(C)
-
-    return np.isclose(np.dot(v1, v2), 0)
-
-
-def isInside(A, B, C, X):
-    min_x = min(A[0], B[0], C[0])
-    max_x = max(A[0], B[0], C[0])
-    min_y = min(A[1], B[1], C[1])
-    max_y = max(A[1], B[1], C[1])
-
-    if min_x <= X[0] <= max_x and min_y <= X[1] <= max_y:
-        return True
-    else:
-        return False
-
-
-def distanceBetweenPoints(A, B):
-    return math.sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
+def is_inside(p1, p2, p3, pX):
+    """
+    Odredjuje da li se tacka X nalazi unutar cetvorougla sa kordinatama p1,p2,p3
+    """
+    coords = np.array([p1, p2, p3])
+    min_coords = coords.min(axis=0)
+    max_coords = coords.max(axis=0)
+    return np.all(min_coords <= pX) and np.all(pX <= max_coords)
 
 
 def distance(A, B):
+    """
+    Odredjuje distancu pomocu Euklidove udaljenosti
+    """
     return np.linalg.norm(np.array(A) - np.array(B))
 
 
-def calculateDiagonal(A, B, C):
-    AB = distanceBetweenPoints(A, B)
-    AC = distanceBetweenPoints(A, C)
-    BC = distanceBetweenPoints(B, C)
+def calculate_diagonal(A, B, C):
+    """
+    Izracunava se duzina izmedju svih tacaka i odredjuje se najveca od njih
+    """
+    AB = distance(A, B)
+    AC = distance(A, C)
+    BC = distance(B, C)
 
     return max(AB, AC, BC)
 
 
 def main():
-    points = loadData("data.txt")
-    if not points or len(points) != 4:
-        print("Nije učitano 4 tačaka iz datoteke")
-        return False
+    points = load_data("data.txt")
+    if points is None:
+        return
 
     A, B, C, X = points
-    if not checkRectagleFromPoints(A, B, C):
+    if not check_rectagle_from_points(A, B, C):
         print("Tačke A, B i C ne prave pravougaonik")
-        return False
+        return
 
     print("Tačke A, B i C prave pravougaonik")
-
-    if isInside(A, B, C, X):
+    if is_inside(A, B, C, X):
         print("Tačka X se nalazi unutar pravougaonika")
     else:
         print("Tačka X se ne nalazi unutar pravougaonika")
 
-    diagonal = calculateDiagonal(A, B, C)
+    diagonal = calculate_diagonal(A, B, C)
     print(f"Dijagonala pravougaonika sa tačkama ABC iznosi {diagonal}")
 
 
